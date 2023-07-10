8134bf82ea0d99bf79818a99bf23c235aff71837
cgnannan <84908809@qq.com>
2023-07-10 16:44:22 +0800

    fine-tune the pinecone calling function

--- Gen_QA.py
+++ Gen_QA.py
@@ -1,51 +1,75 @@
 import openai
 import os
+import pandas as pd
 import pinecone
+import ast
+from tqdm.auto import tqdm
+import time
 
+start_time = time.time()
 
+openai.api_key = os.getenv("OPENAI_API_KEY")
+pinecone_api_key = "b4f05738-8211-4414-a372-d0867ef33c10"
+pinecone_env = "northamerica-northeast1-gcp" 
 
-openai.api_key=os.getenv("OPENAI_API_KEY")
-model="gpt-4-0613"
+model="gpt-3.5-turbo-0613"
 embed_model="text-embedding-ada-002"
-#data = load_dataset('jamescalam/youtube-transcriptions', split='train')
-old_index_name = 'openai'
-new_index_name = 'fifa-world-cup-2022-qatar'
-query = "Brazil's final place in Qatar 2022 World Cup?"
 
-res=openai.Embedding.create(
-    input=[
-        "Sample document text goes here",
-        "there will be several phrases in each batch"
-    ],
-    model=embed_model
-)
+#data = load_dataset('jamescalam/youtube-transcriptions', split='train') 
+csv_file_path = "/Volumes/work/Project/AIGC/OpenAI/Function_Call/data/FIFA_World_Cup_2022.csv"
 
-# initialize connection to pinecone (get API key at app.pinecone.io)
-pinecone.init(
-    api_key="b4f05738-8211-4414-a372-d0867ef33c10",
-    environment="northamerica-northeast1-gcp"  # may be different, check at app.pinecone.io
-)
+pinecone_old_index_name = 'fifa-world-cup-2022-qatar'
+pinecone_new_index_name = 'qatar-2022-fifa-world-cup'
 
-"""# Check whether the index with the same name already exists - if so, delete it
-if old_index_name in pinecone.list_indexes():
-    pinecone.delete_index(old_index_name)
 
-# Creates new index
-if new_index_name not in pinecone.list_indexes():
-    # if does not exist, create index
-    pinecone.create_index(
-        new_index_name,
-        dimension=len(res['data'][0]['embedding']),
-        metric='cosine',
-        metadata_config={'indexed': ['channel_id', 'published']}
+"""def pinecone_csv_upsert(csv_file=None,old_index_name=None,new_index_name=None):
+    "Iterate over the CSV data and upload vectors in a loop"
+    csv_file=csv_file_path
+    csv_data=pd.read_csv(csv_file) #import data from the csv file
+    old_index_name=pinecone_old_index_name
+    new_index_name=pinecone_new_index_name
+
+    # initialize connection to pinecone (get API key at app.pinecone.io)
+    pinecone.init(
+        api_key = pinecone_api_key,
+        environment = pinecone_env   # may be different, check at app.pinecone.io
     )
-# connect to index"""
-index = pinecone.Index(new_index_name)
-# view index stats
-index_stats=index.describe_index_stats()
-print(index_stats)
+
+    res=openai.Embedding.create(
+        input=[
+            "Sample document text goes here",
+            "there will be several phrases in each batch"
+        ],
+        model="text-embedding-ada-002"
+    )
+
+    # Check whether the index with the same name already exists - if so, delete it
+    if old_index_name in pinecone.list_indexes():
+        pinecone.delete_index(old_index_name)
+
+    # Creates new index
+    if new_index_name not in pinecone.list_indexes():
+        # if does not exist, create index
+        pinecone.create_index(
+            new_index_name,
+            dimension=len(res['data'][0]['embedding']),
+            metric='cosine'
+        )
+    # connect to index
+    index=pinecone.Index(new_index_name)
+
+    for idx in tqdm(range(len(csv_data)), desc="Uploading vectors"):
+        text = csv_data.loc[idx, "text"]
+        embedding = ast.literal_eval(csv_data.loc[idx, "embedding"])
+        
+        # Upsert vector and text to Pinecone index
+        index.upsert([(str(idx), embedding, {'text': text})])
+    return print(f"text and embedding have been successfully inserted into Pinecone index\nIndex_Name:{new_index_name}\n{index.describe_index_stats()}")
+
+pinecone_csv_upsert()"""
 
 """
+# Iterate over the non-CSV data and upload vectors in a loop
 new_data = []
 window = 20  # number of sentences to combine
 stride = 4  # number of sentences to 'stride' over, used to create overlap
@@ -105,9 +129,20 @@ for i in tqdm(range(0, len(new_data), batch_size)):
     # upsert to Pinecone
     index.upsert(vectors=to_upsert)"""
 
+
+# initialize connection to pinecone (get API key at app.pinecone.io)
+pinecone.init(
+    api_key = pinecone_api_key,
+    environment = pinecone_env   # may be different, check at app.pinecone.io
+)
+
 limit = 3750
 
-def retrieve(query):
+# connect to index
+index = pinecone.Index(pinecone_new_index_name)
+query = "Who is the top scorer of the 2022 Qatar World Cup?"
+
+def ask_pinecone(query):
     res = openai.Embedding.create(
         input=[query],
         engine=embed_model
@@ -117,7 +152,7 @@ def retrieve(query):
     xq = res['data'][0]['embedding']
 
     # get relevant contexts
-    res = index.query(xq, top_k=5, include_metadata=True)
+    res = index.query(xq, top_k=100, include_metadata=True)
     contexts = [
         x['metadata']['text'] for x in res['matches']
     ]
@@ -127,7 +162,7 @@ def retrieve(query):
         "Context:\n"
     )
     prompt_end = (
-        f"\n\nQuestion: {query}\nAnswer:"
+        f"\n\nQuestion: {query}"
     )
     # append contexts until hitting limit
     for i in range(1, len(contexts)):
@@ -144,13 +179,7 @@ def retrieve(query):
                 "\n\n---\n\n".join(contexts) +
                 prompt_end
             )
-    return prompt
-    
-query_with_contexts=retrieve(query)
-print(query_with_contexts)
-
-def complete(prompt):
-    res=openai.ChatCompletion.create(
+    response=openai.ChatCompletion.create(
         model=model,
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
@@ -159,6 +188,10 @@ def complete(prompt):
         temperature=0,
         max_tokens=256
     )
-    return res['choices'][0]['message']['content'].strip()
+    return response['choices'][0]['message']['content'].strip()
 
-print(complete(query_with_contexts))
+print(ask_pinecone(query))
+
+end_time=time.time()
+execution_time = end_time - start_time
+print(execution_time)
